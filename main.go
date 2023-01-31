2c26f26c8ca889ef70800f2669633221bfb171e4
notfirefox <115221458+notfirefox@users.noreply.github.com>
2023-01-31 09:59:39 +0100

    Add basic code

--- main.go
+++ main.go
@@ -2,7 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-  fmt.Println("Hello, World!")
+type function func(int) int
+
+type interval struct {
+	a int
+	b int
 }
 
+func function_impl(x int) int {
+	return (x * x) + (2 * x)
+}
+
+func bisect(fn function, i interval) int {
+	var mid int = (i.a + i.b) / 2
+	var fmid int = fn(mid)
+	if fmid == 0 {
+		return mid
+	} else if fmid > 0 {
+		return bisect(fn, interval{i.a, mid})
+	} else {
+		return bisect(fn, interval{mid, i.b})
+	}
+}
+
+func main() {
+	var f function = function_impl
+	var i interval = interval{-3, -1}
+	fmt.Println(bisect(f, i))
+}
