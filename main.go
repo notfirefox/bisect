ed21bc2312d9eee04a74b3e12850fb5b14aa0181
notfirefox <115221458+notfirefox@users.noreply.github.com>
2023-01-31 12:33:05 +0100

    Update

--- main.go
+++ main.go
@@ -9,24 +9,32 @@ type interval struct {
 	b int
 }
 
-func function_impl(x int) int {
-	return (x * x) + (2 * x)
-}
-
 func bisect(fn function, i interval) int {
 	var mid int = (i.a + i.b) / 2
 	var fmid int = fn(mid)
 	if fmid == 0 {
 		return mid
-	} else if fmid > 0 {
-		return bisect(fn, interval{i.a, mid})
 	} else {
-		return bisect(fn, interval{mid, i.b})
+		var fa int = fn(i.a)
+		var fb int = fn(i.b)
+		if fa < 0 && fmid > 0 {
+			return bisect(fn, interval{i.a, mid})
+		}
+		if fa > 0 && fmid < 0 {
+			return bisect(fn, interval{i.a, mid})
+		}
+		if fb < 0 && fmid > 0 {
+			return bisect(fn, interval{mid, i.b})
+		}
+		if fb > 0 && fmid < 0 {
+			return bisect(fn, interval{mid, i.b})
+		}
+		return 666
 	}
 }
 
 func main() {
-	var f function = function_impl
-	var i interval = interval{-3, -1}
-	fmt.Println(bisect(f, i))
+	var i interval = interval{-1, 1}
+	var z = bisect(func(x int) int { return (x * x) + (2 * x) }, i)
+	fmt.Println(z)
 }
